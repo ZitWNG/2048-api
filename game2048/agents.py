56f81ab262a3059955fc4f51588737f326b77c44
ZitWNG <44541237+ZitWNG@users.noreply.github.com>
2019-01-13 18:55:08 +0800

    Add files via upload

--- game2048/agents.py
+++ game2048/agents.py
@@ -22,7 +22,7 @@ class Agent:
                     self.display.display(self.game)
 
     def step(self):
-        direction = int(input("0: left, 1: down, 2: right, 3: up = ")) % 4
+        direction = int(float(input("0: left, 1: down, 2: right, 3: up = "))) % 4
         return direction
 
 
@@ -46,3 +46,54 @@ class ExpectiMaxAgent(Agent):
     def step(self):
         direction = self.search_func(self.game.board)
         return direction
+
+
+
+# my agent
+
+
+def board_transform(board_to_transe,board_dimension, gamesize=4):
+    """transform the board by one-hot encoding"""
+    if board_dimension == 3:
+        inputboard = np.zeros((1, 16, 12))
+        for p in range(4):
+            for q in range(4):
+                num = board_to_transe[p, q]
+                if num == 0:
+                    inputboard[0, 4*p+q, 0] = 1
+                else:
+                    inputboard[0, 4*p+q, int(np.log2(num))] = 1 
+    elif board_dimension==4:
+        inputboard = np.zeros(( 1, 4, 4, 12))
+        for p in range(4):
+            for q in range(4):
+                num = board_to_transe[p, q]
+                if num == 0:
+                    inputboard[0, p, q, 0] = 1
+                else:
+                    inputboard[ 0, p, q, int(np.log2(num))] = 1
+    else:
+        print('OH NO! you data denmension is not ture!')
+
+    return inputboard
+
+
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+from keras.models import load_model
+
+class ZitAgent(Agent):
+    """my agent"""
+    def __init__(self, game, display = None):
+        super().__init__(game, display)
+        self.zmodel = load_model('/home/srtp/ML/2048-api-master/model0002.h5')
+        self.zmodel.summary()
+
+    def step(self):
+        #this_board = board_transform(self.game.board, 4)
+        
+        one_hot_direction = self.zmodel.predict(np.array(board_transform(self.game.board,4)))
+        direction = np.argmax(one_hot_direction[0])
+        return direction
+
